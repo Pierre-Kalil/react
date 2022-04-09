243a8d411befe43062a820457ae0697cc74c842b
Pierre Kalil <pierrekalil1@gmail.com>
2022-04-09 19:48:04 -0300

    feat: add global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+    font-family: 'Heebo', sans-serif;
+    font-weight: normal;
+  }
+
+  li{
+    list-style: none;
+  }
+  a{
+    text-decoration: none;
+  }
+  button{
+    cursor: pointer;
+  }
+  svg{
+    cursor: pointer;
+  }
+ 
+  `;
